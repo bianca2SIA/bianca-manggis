c28304951b05cf60387ad5cc614eba168b4399f5
bianca2SIA <bianca24si@pcr.ac.id>
2025-10-02 08:06:13 +0700

    modif halaman about

--- resources/views/halaman-about.blade.php
+++ resources/views/halaman-about.blade.php
@@ -5,6 +5,6 @@
 </head>
 <body>
     <h1>Ini adalah halaman About Mahasiswa</h1>
-    <p>Laravel membuat development lebih cepat 🚀</p>
+    <p>Laravel membuat development lebih cepat </p>
 </body>
 </html>
